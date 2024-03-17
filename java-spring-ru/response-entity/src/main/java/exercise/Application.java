8ba72b583fe09966f3237769bbf14ad1586be708
Lindelor <leontev150@mail.ru>
2024-03-17 15:54:06 +0400

    ResponseEntity homework is done

--- java-spring-ru/response-entity/src/main/java/exercise/Application.java
+++ java-spring-ru/response-entity/src/main/java/exercise/Application.java
@@ -1,10 +1,10 @@
 package exercise;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,9 +28,40 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    // BEGIN
-    
-    // END
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> index(@RequestParam(defaultValue = "1") Integer page,
+                                            @RequestParam(defaultValue = "10") Integer limit) {
+        var result = posts.stream().skip((long) (page - 1) * limit).limit(limit).toList();
+        var headers = new HttpHeaders();
+        headers.add("X-Total-Count", String.valueOf(posts.size()));
+        return new ResponseEntity<>(result, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Post> show(@PathVariable String id) {
+        var result = posts.stream().filter(p -> p.getId().equals(id)).findFirst();
+        return result.map(post -> new ResponseEntity<>(post, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/posts")
+    public ResponseEntity<Post> create(@RequestBody Post post) {
+        posts.add(post);
+        return new ResponseEntity<>(post, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<Post> update(@PathVariable String id, @RequestBody Post data) {
+        var maybePost = posts.stream().filter(p -> p.getId().equals(id)).findFirst();
+
+        if (maybePost.isPresent()) {
+            var post = maybePost.get();
+            post.setId(data.getId());
+            post.setBody(data.getBody());
+            post.setTitle(data.getTitle());
+            return new ResponseEntity<>(post, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
     @DeleteMapping("/posts/{id}")
     public void destroy(@PathVariable String id) {
