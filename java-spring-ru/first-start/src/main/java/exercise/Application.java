30910a98e6fff89b84f2ef1c481ae6e954cfbe4d
Lindelor <leontev150@mail.ru>
2024-03-17 08:57:39 +0400

    homework is done

--- java-spring-ru/first-start/src/main/java/exercise/Application.java
+++ java-spring-ru/first-start/src/main/java/exercise/Application.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-// BEGIN
-
-// END
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @GetMapping("/about")
+    String about() {
+        return "Welcome to Hexlet! Test autorestart";
+    }
+}
