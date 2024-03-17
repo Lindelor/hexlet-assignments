5e3c1325f6f0dad43e7af2d43d8cdba588bb7cf8
Lindelor <leontev150@mail.ru>
2024-03-17 11:02:54 +0400

    crud homework is done

--- java-spring-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -26,7 +26,40 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    // BEGIN
-    
-    // END
+    @GetMapping("/posts")
+    public List<Post> index(@RequestParam(defaultValue = "1") Integer page,
+                            @RequestParam(defaultValue = "10") Integer limit) {
+        return posts.stream().skip(((long) page - 1) * limit).limit(limit).toList();
+    }
+
+    @GetMapping("/posts/{id}")
+    public Optional<Post> show(@PathVariable String id) {
+        return posts.stream().filter(p -> p.getId().equals(id)).findFirst();
+    }
+
+    @PostMapping("/posts")
+    public Post create(@RequestBody Post post) {
+        posts.add(post);
+        return post;
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
+
+    @PutMapping("/posts/{id}")
+    public Post update(@PathVariable String id, @RequestBody Post data) {
+        var optPost = posts.stream().filter(p -> p.getId().equals(id)).findFirst();
+
+        if (optPost.isPresent()) {
+            var post = optPost.get();
+
+            post.setId(data.getId());
+            post.setBody(data.getBody());
+            post.setTitle(data.getTitle());
+        }
+        return data;
+    }
+
 }
