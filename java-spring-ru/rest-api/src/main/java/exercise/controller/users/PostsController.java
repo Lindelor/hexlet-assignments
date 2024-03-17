d68c7034fc6580cbf21eb4009a01f981e51637b4
Lindelor <leontev150@mail.ru>
2024-03-17 16:27:40 +0400

    Rest homework is done

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -2,9 +2,9 @@ package exercise.controller.users;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +13,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import exercise.model.Post;
 import exercise.Data;
 
-// BEGIN
+@RestController
+@RequestMapping("/api")
+public class PostsController {
+    private List<Post> posts = Data.getPosts();
+    @GetMapping("/users/{id}/posts")
+    public ResponseEntity<List<Post>> show(@PathVariable int id) {
+        var result = posts.stream().filter(p -> p.getUserId() == id).toList();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
-// END
+    @PostMapping("/users/{id}/posts")
+    public ResponseEntity<Post> create(@PathVariable int id, @RequestBody Post post) {
+        post.setUserId(id);
+        posts.add(post);
+        return new ResponseEntity<>(post, HttpStatus.CREATED);
+    }
+}
