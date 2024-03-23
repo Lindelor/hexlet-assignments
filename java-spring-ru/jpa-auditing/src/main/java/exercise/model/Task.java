8e73981859e41f83e05935d0b334e0be781db05a
Lindelor <leontev150@mail.ru>
2024-03-23 10:34:46 +0400

    jpa-auditing hw is done

---
+++ java-spring-ru/jpa-auditing/src/main/java/exercise/model/Task.java
@@ -0,0 +1,33 @@
+package exercise.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tasks")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
+public class Task {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private long id;
+    private String title;
+    private String description;
+    @CreatedDate
+    private LocalDate createdAt;
+    @LastModifiedDate
+    private LocalDate updatedAt;
+}
